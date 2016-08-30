4fdfd31936610dcc23f2667b3e7265f3e074b654
AlanTai <rainman.tai@gmail.com>
2016-08-29 20:23:28 -0700

    update map mechanism

--- public/js/index.js
+++ public/js/index.js
@@ -19,16 +19,27 @@
 
     // global values
     window.indexApp.value('APP_VALUES', {
-      EMAIL: 'gogistics@gogistics-tw.com'
+      EMAIL: 'gogistics@gogistics-tw.com',
+      GEO_LOCATION: null
     });
 
     window.indexApp.config(function(){
       console.log('start to config');
     });
 
-    window.indexApp.run(function(){
+    window.indexApp.run(['APP_VALUES', function(APP_VALUES){
       console.log('start to run');
-    });
+      if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(setGeoLocation);
+      }else{
+        alert('Your Browser Does not support Geolocation');
+      }
+      function setGeoLocation(position){
+        APP_VALUES.GEO_LOCATION = {};
+        APP_VALUES.GEO_LOCATION['lat'] = position.coords.latitude;
+        APP_VALUES.GEO_LOCATION['lng'] = position.coords.longitude;
+      }
+    }]);
 
     window.indexApp.factory('leafletMap', function(){
       var leafletMapTiles = new L.TileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png');
@@ -37,14 +48,25 @@
 
     window.indexApp.service('dataProvider', function($http, APP_VALUES){
       this.geoQuery = function(arg_url, arg_headers, arg_data){
-        //
-      }
+        return $http({
+           url: arg_url,
+           method: 'POST',
+           data: arg_data,
+           headers: arg_headers
+        });
+      };
     });
 
-    window.indexApp.controller('indexCtrl', ['$scope', 'leafletMap', 'dataProvider', function($scope, leafletMap, dataProvider){
+    window.indexApp.controller('indexCtrl', ['$scope', 'APP_VALUES', 'leafletMap', 'dataProvider', function($scope, APP_VALUES, leafletMap, dataProvider){
       var ctrl = this;
-      ctrlinit = function(){
-        leafletMap.on("viewreset", reset);
+      ctrl.init = function(){
+        if(APP_VALUES.GEO_LOCATION) leafletMap.setView(new L.LatLng(APP_VALUES.GEO_LOCATION.lat, APP_VALUES.GEO_LOCATION.lng), 15);
+        ctrl.svg = d3.select(leafletMap.getPanes().overlayPane).append('svg');
+        ctrl.g = ctrl.svg.append('g').attr('class', 'leaflet-zoom-hide');
+      };
+
+      ctrl.submitGeoQuery = function(){
+        // incomplete
       }
       console.log('index ctrl is ready');
     }]);
